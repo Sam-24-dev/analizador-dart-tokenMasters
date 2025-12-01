36703465fd7f958e54a43bc7a5a4c402cca2b2dc
Mateo Mayorga <mmayorgan@gmail.com>
2025-12-01 15:54:05 -0500

    Modificacion a mi algoritmo

--- algoritmos_prueba/algoritmo_mateo.dart
+++ algoritmos_prueba/algoritmo_mateo.dart
@@ -15,7 +15,8 @@ int fibonacci(int n) {
 }
 
 void main() {
-    int n = 9;
-    print("El numero de la sucesion de fibonacci en la posicion 9 es: ");
-    print(fibonacci(n));
+    int n = [0,1,2,3,4,5,6,7,8,9]; 
+    for (int i = 0; i < n.length; i++){
+        print(fibonacci(i));
+    }
 }
